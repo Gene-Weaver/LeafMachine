ed13a5faca4411dd48f395fed76ef7852ca06c53
Will <30776938+Gene-Weaver@users.noreply.github.com>
2018-12-02 17:58:27 -0700

    Updated LeafMachine.m

--- LeafMachine.m
+++ LeafMachine.m
@@ -8,4 +8,12 @@
 %%%     University of Colorado, Boulder
 %%%     Department of Ecology and Evolutionary Biology
 
+addpath(genpath('Img'));
+addpath(genpath('Leaf_Data'));
+addpath(genpath('Processed_Images'));
+addpath(genpath('Raw_Images'));
+addpath(genpath('Ruler'));
+addpath(genpath('Networks'));
+addpath(genpath('SandboxFunctions'));
+addpath(genpath('Training_Images'));
 GUI_Auto_Distance
