3f536d913cfad04afde3fbb93e12e57bb347c727
Will <30776938+Gene-Weaver@users.noreply.github.com>
2018-12-02 17:54:53 -0700

    Added functional batch processing, unstable

---
+++ LeafMachine.m
@@ -0,0 +1,11 @@
+
+%%%     Developed for LeafMachine.org
+%%%
+%%%     William Weaver
+%%%
+%%%     Contact: LeafMachine@colorado.edu
+%%%
+%%%     University of Colorado, Boulder
+%%%     Department of Ecology and Evolutionary Biology
+
+GUI_Auto_Distance
