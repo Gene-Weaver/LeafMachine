1fa2fcde63684e9181b8fa05412ae0f54632e7da
Will <william.weaver@colorado.edu>
2019-09-27 14:51:06 -0400

    new opening window

--- LeafMachine.m
+++ LeafMachine.m
@@ -9,11 +9,18 @@
 %%%     Department of Ecology and Evolutionary Biology
 
 addpath(genpath('Img'));
-addpath(genpath('Leaf_Data'));
-addpath(genpath('Processed_Images'));
-addpath(genpath('Raw_Images'));
-addpath(genpath('Ruler'));
 addpath(genpath('Networks'));
 addpath(genpath('SandboxFunctions'));
-addpath(genpath('Training_Images'));
-GUI_Auto_Distance
+LeafMachineInitGUI
+
+
+%%% For Manual version of LeafMachine...
+% addpath(genpath('Img'));
+% addpath(genpath('Leaf_Data'));
+% addpath(genpath('Processed_Images'));
+% addpath(genpath('Raw_Images'));
+% addpath(genpath('Ruler'));
+% addpath(genpath('Networks'));
+% addpath(genpath('SandboxFunctions'));
+% addpath(genpath('Training_Images'));
+% GUI_Auto_Distance
